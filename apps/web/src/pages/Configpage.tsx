7b1fdb5b760b1218f4820378c3bb24ad65e69fd8
Kensaa <kensadevs@gmail.com>
2024-02-16 11:53:41 +0100

    added config editor

--- apps/web/src/pages/Configpage.tsx
+++ apps/web/src/pages/Configpage.tsx
@@ -3,11 +3,19 @@ import AppNavbar from '../components/AppNavbar'
 import { useEffect, useState } from 'react'
 import type { PrinterConfig } from 'printer-types'
 import LoadingSpinner from '../components/LoadingSpinner'
+import AceEditor from 'react-ace'
+import Button from '../components/Button'
+
+import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/mode-json5'
+import { Alert } from 'react-bootstrap'
 
 export default function Configpage() {
     const address = useAddress()
     const [config, setConfig] = useState<PrinterConfig | undefined>()
+    const [value, setValue] = useState('')
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
 
     useEffect(() => {
         setLoading(true)
@@ -19,6 +27,20 @@ export default function Configpage() {
             })
     }, [address])
 
+    const save = () => {
+        fetch(`${address}/config`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: value
+        }).then(res => {
+            if (res.status === 400) {
+                setError('Invalid Config')
+            }
+        })
+    }
+
     return (
         <div className='page'>
             <AppNavbar />
@@ -26,7 +48,28 @@ export default function Configpage() {
                 {loading ? (
                     <LoadingSpinner style={{ width: '100%', height: '100%' }} />
                 ) : (
-                    <pre>{JSON.stringify(config, null, 4)}</pre>
+                    <>
+                        {error && (
+                            <Alert
+                                dismissible
+                                variant='danger'
+                                onClose={() => setError('')}
+                            >
+                                {error}
+                            </Alert>
+                        )}
+                        <h1>WIP</h1>
+                        <AceEditor
+                            mode='json5'
+                            theme='github'
+                            onChange={val => setValue(val)}
+                            defaultValue={JSON.stringify(config, null, 2)}
+                            value={value}
+                            name='ConfigEditor'
+                            editorProps={{ $blockScrolling: true }}
+                        />
+                        <Button onClick={save}>Save</Button>
+                    </>
                 )}
             </div>
         </div>
