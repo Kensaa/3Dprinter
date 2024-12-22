3824dc18a69431eb115790952affbfb288e1f19e
Kensaa <54020933+Kensaa@users.noreply.github.com>
2024-12-22 16:19:10 +0100

    added a form to move all printers into a line

--- apps/web/src/modals/RemoteControlModal.tsx
+++ apps/web/src/modals/RemoteControlModal.tsx
@@ -1,4 +1,4 @@
-import { createElement, useEffect, useState } from 'react'
+import { createElement, useMemo, useState } from 'react'
 import { Form, Modal } from 'react-bootstrap'
 import Button from '../components/Button'
 import { Printer } from '../utils/types'
@@ -27,17 +27,9 @@ export default function RemoteControlModal({
     hide,
     printers
 }: RemoteControlModalProps) {
-    const [name, setName] = useState('')
-
-    useEffect(() => {
-        if (printers.length === 0) {
-            hide()
-        } else {
-            setName(
-                printers.length > 1 ? 'Multiple Printers' : printers[0].label
-            )
-        }
-    }, [printers, hide])
+    const name = useMemo(() => {
+        return printers.length > 1 ? 'Multiple Printers' : printers[0].label
+    }, [printers])
 
     return (
         <Modal show={show} onHide={hide}>
@@ -109,6 +101,11 @@ export default function RemoteControlModal({
                 </div>
                 <GoToForm printers={printers} />
                 <HeadToForm printers={printers} />
+                {printers.length > 1 && (
+                    <>
+                        <LineForm printers={printers} />
+                    </>
+                )}
             </Modal.Body>
         </Modal>
     )
@@ -268,6 +265,119 @@ function HeadToForm({ printers }: GoToFormProps) {
             </Form.Select>
             <Button variant='outline-primary' type='submit'>
                 Head To
+            </Button>
+        </Form>
+    )
+}
+
+function LineForm({ printers }: GoToFormProps) {
+    const [x, setX] = useState<number | undefined>()
+    const [y, setY] = useState<number | undefined>()
+    const [z, setZ] = useState<number | undefined>()
+    const [heading, setHeading] = useState<number>(0)
+
+    const address = useAddress()
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        e.stopPropagation()
+        if (!(x && y && z)) {
+            return
+        }
+        const currentPos = [x, y, z]
+        for (const printer of printers) {
+            fetch(`${address}/remote`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    printer: printer.id,
+                    command: 'goTo',
+                    data: currentPos
+                })
+            })
+
+            switch (heading) {
+                case 0:
+                    currentPos[0] += 1
+                    break
+                case 1:
+                    currentPos[2] += 1
+                    break
+                case 2:
+                    currentPos[0] -= 1
+                    break
+                case 3:
+                    currentPos[2] -= 1
+                    break
+                default:
+                    console.error('invalid heading ' + heading)
+                    return
+            }
+        }
+    }
+
+    const pasteShortcut = (e: React.ClipboardEvent) => {
+        const data = e.clipboardData.getData('Text')
+        if (isNaN(+data)) {
+            e.preventDefault()
+            const split = data.trim().split(' ')
+            if (split.length !== 3) return
+            setX(parseInt(split[0].trim()))
+            setY(parseInt(split[1].trim()))
+            setZ(parseInt(split[2].trim()))
+        }
+    }
+
+    return (
+        <Form
+            onSubmit={submit}
+            className='d-flex flex-column align-items-center w-100 mt-3'
+        >
+            <Form.Label>Move printers in a line starting from: </Form.Label>
+            <div className='d-flex flex-row w-75 mx-1'>
+                <Form.Control
+                    type='number'
+                    placeholder='X'
+                    value={x}
+                    onChange={e => setX(parseInt(e.currentTarget.value))}
+                    onPaste={pasteShortcut}
+                />
+                <Form.Control
+                    type='number'
+                    placeholder='Y'
+                    value={y}
+                    onChange={e => setY(parseInt(e.currentTarget.value))}
+                    onPaste={pasteShortcut}
+                />
+                <Form.Control
+                    type='number'
+                    placeholder='Z'
+                    value={z}
+                    onChange={e => setZ(parseInt(e.currentTarget.value))}
+                    onPaste={pasteShortcut}
+                />
+            </div>
+            <Form.Label>Heading: </Form.Label>
+            <Form.Select
+                value={heading}
+                onChange={e => setHeading(parseInt(e.target.value))}
+                className='w-75 mx-1'
+            >
+                {headings.map((heading, i) => (
+                    <option key={i} value={i}>
+                        {heading}
+                    </option>
+                ))}
+            </Form.Select>
+            <Button
+                variant='outline-primary'
+                type='submit'
+                className='mt-1'
+                disabled={!(x && y && z)}
+            >
+                Go
             </Button>
         </Form>
     )
